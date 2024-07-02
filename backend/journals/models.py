a1ec855b26ad052ee1bcd7812e6a940bf84de04a
AndrewIndeche <indecheandrew@outlook.com>
2024-07-02 20:12:06 +0300

    create journal model

--- backend/journals/models.py
+++ backend/journals/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
+class JournalEntry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='entries')
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    category = models.CharField(max_length=100, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
