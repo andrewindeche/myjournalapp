e9358984ca7bd0c18325f059b78bcada92c46807
AndrewIndeche <indecheandrew@outlook.com>
2024-07-02 20:42:52 +0300

    register journal views

--- backend/journals/views.py
+++ backend/journals/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import JournalEntry
+from .serializers import  JournalEntrySerializer
 
 # Create your views here.
+class JournalEntryView(generics.ListCreateAPIView):
+    serializer_class = JournalEntrySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return JournalEntry.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
