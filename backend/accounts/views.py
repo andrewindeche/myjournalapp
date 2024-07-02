92b6dde370241c6332cd2c585b43e53a5ea5e89a
AndrewIndeche <indecheandrew@outlook.com>
2024-07-02 20:40:39 +0300

    register User views

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .models import User
+from .serializers import UserSerializer, UserProfileSerializer
 
 # Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class LoginView(TokenObtainPairView):
+    pass
+
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
