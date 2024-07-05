d87e3dded822298bc2e66818e0be2bd148a03caf
AndrewIndeche <indecheandrew@outlook.com>
2024-07-05 16:23:23 +0300

    update style and add pressables

--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -1,30 +1,68 @@
 import React from 'react';
-import { Text, View, StyleSheet,Button } from 'react-native';
+import { Text, View, StyleSheet, Pressable } from 'react-native';
 
 export default function HomeScreen() {
     return(
+
+    <View style={styles.section}>
     <View style={styles.container}>
     <View style={styles.header}>
-        <Text>Everyday has a Story!</Text>
-        <Text>Write Yours</Text>
+        <Text style={styles.title}>Everyday has a Story!</Text>
+        <Text style={styles.title}>Write Yours</Text>
+        <Text style={styles.subtitle}>Dive into Creativity</Text>
+        <Text style={styles.subtitle}> Document Your Imagination</Text>
       </View>
-      <View>
-          <Button title="SignIn" onPress={() => {}} />
-          <Button title="Register" onPress={() => {}} />
+      <View style={styles.footer}>
+      <Pressable style={styles.pressable} onPress={() => console.log('Pressed')}>
+      <Text style={styles.text}>Sign In</Text>
+    </Pressable>
+    <Pressable style={styles.pressable} onPress={() => console.log('Pressed')}>
+      <Text style={styles.text}>Register</Text>
+    </Pressable>
+        </View>
         </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+  section: {
+    backgroundColor: '#fff',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  pressable: {
+    padding: 10,
+    backgroundColor: '#DDDDDD',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: 'black',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
   header: {
-    backgroundColor: '#fff',
     alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    fontSize: 18,
+    color: '#555',
+  },
+  footer: {
+    backgroundColor: '#003366',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
     padding: 20,
   },
 });
