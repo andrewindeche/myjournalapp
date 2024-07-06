deeb33eeaf65cb59bbdfb7490ede04e9b0e4bae0
AndrewIndeche <indecheandrew@outlook.com>
2024-07-07 00:28:34 +0300

    add redux to dependencies

--- frontend/app/src/screens/HomeScreen.tsx
+++ frontend/app/src/screens/HomeScreen.tsx
@@ -47,7 +47,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    fontFamily: 'mulish-regular',
   },
   pressable: {
     padding: 10,
