c50a9b4b9f518c714d89bb1029cd9b44441def0a
andrew <indecheandrew@gmail.com>
2024-08-09 17:16:58 +0300

    update delete image function

--- frontend/app/src/screens/JournalEntryScreen.tsx
+++ frontend/app/src/screens/JournalEntryScreen.tsx
@@ -76,6 +76,11 @@ const JournalEntryScreen: React.FC = () => {
     }
   }, [editMode, currentEntry]);
 
+  useEffect(() => {
+    console.log('Current Entry:', currentEntry);
+    console.log('Image URI:', currentEntry?.content_image?.uri);
+  }, [currentEntry]);
+
   const handleImageUpload = () => {
     const options: ImageLibraryOptions = { mediaType: "photo" };
     launchImageLibrary(options, (response) => {
@@ -233,15 +238,24 @@ const JournalEntryScreen: React.FC = () => {
       });
   };
 
-  const handleDeleteImage = () => {
+  const handleDeleteImage = async () => {
+    console.log("handleDeleteImage called");
     if (editEntryId && currentEntry) {
       const updatedEntry = {
         ...currentEntry,
         content_image: null,
       };
 
-      dispatch(updateJournalEntry({ id: editEntryId, ...updatedEntry }));
-      setImageUri(null);
+      try {
+        await dispatch(
+          updateJournalEntry({ id: editEntryId, ...updatedEntry }),
+        ).unwrap();
+        setImageUri(null);
+      } catch (error) {
+        console.error("Failed to update journal entry:", error);
+      }
+    } else {
+      console.log("No entry to update or invalid editEntryId");
     }
   };
 
@@ -303,7 +317,7 @@ const JournalEntryScreen: React.FC = () => {
                     {currentEntry.content_text}
                   </Text>
                 )}
-                {currentEntry.content_image?.uri ? (
+                {currentEntry?.content_image?.uri ? (
                   <Image
                     source={{ uri: currentEntry.content_image.uri }}
                     style={styles.entryImage}
