d91f656365641172c14d86e3e194f0ca966d599a
andrewindeche <indecheandrew@gmail.com>
2026-04-13 13:21:05 +0300

    remove cypress

--- frontend/cypress/plugins/index.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" /> 
-
-/** 
- * @type {Cypress.PluginConfig} 
- */ 
-module.exports = (on, config) => { 
-// Add your custom plugins or configuration here 
-return config; 
-};
